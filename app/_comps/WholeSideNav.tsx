a47880650f57d2952a50f8e4cc29e187bdcd40e9
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2024-05-12 09:18:02 +0300

    Change kebab-case to camelCase

--- app/_comps/WholeSideNav.tsx
+++ app/_comps/WholeSideNav.tsx
@@ -79,13 +79,13 @@ function BottomPhotoOfSideNav() {
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
-						stroke-width="1.5"
+						strokeWidth="1.5"
 						stroke="currentColor"
 						className="w-5 h-5"
 					>
 						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 							d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
 						/>
 					</svg>
@@ -107,9 +107,9 @@ function SideNavSearch() {
 					<path
 						d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
 						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					></path>
 				</svg>
 			</span>
