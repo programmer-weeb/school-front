907bb089468d5a474fef02f8ae71a0f04d59866e
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2024-05-15 14:41:34 +0300

    Make TabNavigation cleaner

--- app/_comps/LinksFromChat.tsx
+++ app/_comps/LinksFromChat.tsx
@@ -11,25 +11,9 @@ interface LinksProps {
 	studentId?: any;
 }
 
-// export default function LinksFromChatgpt({ tabs, activeTab, setActiveTab }: LinksProps) {
-	
-//   return (
-//     <div>
-//       <TabNavigation tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
-//     </div>
-//   );
-// }
-
-// const TabNavigation = ({ tabs, activeTab, setActiveTab }: {
-//   tabs: { label: string; href: string }[];
-//   activeTab: string;
-//   setActiveTab: (tab: string) => void;
-// }) => {
 export const TabNavigation = ({ tabs, activeTab, studentId }: LinksProps) => {
-	// get the current path
+
 	const pathname = usePathname();
-  // const { studentId } = params;
-	// console.log(studentId)
 
 	console.log({pathname})
   return (
@@ -37,13 +21,8 @@ export const TabNavigation = ({ tabs, activeTab, studentId }: LinksProps) => {
       <nav className="flex gap-6" aria-label="Tabs">
         {tabs.map(tab => (
           <Link
-            // href={tab.href}
-            // href='google'
-						// href={`${pathname}/${tab.href}`}
-						// href={`/students/${studentId}${tab.href}`}
 						href={`/students/${studentId}${tab.href}`}
             key={tab.label}
-            // onClick={() => setActiveTab(tab.label)} // Set active tab on click
             className={`shrink-0 rounded-lg p-2 text-sm font-medium ${activeTab === tab.label ? 'bg-sky-100 text-sky-600' : 'text-gray-500 hover:bg-gray-50 hover:text-gray-700'}`}
           >
             {tab.label}
